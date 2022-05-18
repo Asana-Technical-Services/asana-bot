3541f3719bfa6815a0cb8d38a72fd7c892370b13
Ben Graney Green <bengraneygreen@asana.com>
2022-05-18 13:51:07 -0700

    Update asana.js

--- src/pages/api/asana.js
+++ src/pages/api/asana.js
@@ -10,7 +10,7 @@ export default async function asanaAPI(req, res) {
 
   if (!session?.access_token) {
     res.json({ error: "user error" });
-  } else {
+  } else if (data.method == "POST") {
     try {
       // generate random ID to send
       let webhookCode = randomUUID();
@@ -64,5 +64,26 @@ export default async function asanaAPI(req, res) {
     } catch (err) {
       res.json({ error: err.message });
     }
+  } else {
+    axios({
+      method: data.method,
+      url: "https://app.asana.com/api/1.0/" + data.endpoint,
+      params: data.params,
+      headers: {
+        Authorization: process.env.ASANA_TOKEN,
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      data: data.data,
+    })
+      .then((response) => {
+        if (response.status === 200 || response.status === 201) {
+          res.send(response.data);
+        } else {
+          res.json({ error: response.status + " Error - " + response.data });
+        }
+      })
+      .catch((err) => {
+        res.json({ error: err.message });
+      });
   }
 }
